3139c9e15d066ce13146e2953ee551bf30ce3f2d
Anna Hoffa <60316298+annahoffa@users.noreply.github.com>
2021-10-22 01:19:40 +0200

    Add the option to modify existing dashboard list
    - Modify products' quantity
    - Delete products from list

--- react-app/src/components/dashboard-product/dashboard-product.component.jsx
+++ react-app/src/components/dashboard-product/dashboard-product.component.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import apiCall from '../../utils/apiCall';
 
-import { Typography } from '@material-ui/core';
+import { Button, Popover, Typography, TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, InputAdornment } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import EditIcon from '@material-ui/icons/Edit';
 
 import './dashboard-product.styles.scss';
 
 
 const DashboardProduct = ({ product }) => {
+
+  const modifyProductQuantity = () => {
+    apiCall(`/api/user/editProduct?productId=${product.id}&quantity=${productQuantity}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(() => {window.location.reload()})
+    .catch(error => console.log(error));
+
+    closeQuantityDialog();
+  };
 
   const deleteProductFromDashboard = () => {
     apiCall(`/api/user/removeProduct?productId=${product.id}`, {
@@ -21,16 +35,68 @@ const DashboardProduct = ({ product }) => {
     .catch(error => console.log(error));
   };
 
+  const [productQuantity, setProductQuantity] = useState('');
+
+  const handleChange = (event) => {
+    setProductQuantity(event.target.value);
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const openQuantityDialog = () => {
+    setOpen(true);
+  };
+
+  const closeQuantityDialog = () => {
+    setOpen(false);
+  };
+
+
   return (
     <div className={`dashboard-product ${product.allergenWarning ? 'dashboard-product-with-warning' : ''}`}>
       <div className='product-title'>
         <Typography noWrap>
           {product.name} ({product.quantity})
         </Typography>
-        <IconButton onClick={deleteProductFromDashboard} title='Usuń produkt'
-          aria-label='Delete product from dashboard'>
-          <RemoveCircleIcon />
-        </IconButton>
+        <div className='icons'>
+          <IconButton onClick={openQuantityDialog} title='Zmień ilość'
+            aria-label="Modify product's quantity">
+            <EditIcon />
+          </IconButton>
+
+          {/*Hidden modification dialog*/}
+          <Dialog open={open} onClose={closeQuantityDialog}>
+            <DialogContent>
+              <DialogContentText>
+                Zmień ilość produktu:
+              </DialogContentText>
+              <TextField
+                autoFocus
+                variant='standard'
+                value={productQuantity}
+                onChange={handleChange}
+                type='number'
+                inputProps={{
+                  min: 0,
+                }}
+                InputProps={{
+                  startAdornment: <InputAdornment position='start'>mg</InputAdornment>,
+                }}
+                margin='dense'
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={closeQuantityDialog}>Anuluj</Button>
+              <Button onClick={modifyProductQuantity}>Zapisz</Button>
+            </DialogActions>
+          </Dialog>
+
+          <IconButton onClick={deleteProductFromDashboard} title='Usuń produkt'
+            aria-label='Delete product from dashboard'>
+            <RemoveCircleIcon />
+          </IconButton>
+        </div>
       </div>
       <ul className='nutrients'>
         <li>Kcal: {product.nutrients?.kcal || '\u2014'}</li>
@@ -39,6 +105,9 @@ const DashboardProduct = ({ product }) => {
         <li>{product.nutrients?.fat || '\u2014'}</li>
         <li>{product.nutrients?.fiber || '\u2014'}</li>
       </ul>
+      <div className='quantity'>
+        <span>Ilość: {product.quantity || '\u003F'}</span>
+      </div>
     </div>
   );
 };
