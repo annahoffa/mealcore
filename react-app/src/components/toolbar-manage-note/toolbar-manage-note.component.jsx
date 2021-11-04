eb52bd52fb6c142807597a83e20f3bd2498cff1d
annahoffa <60316298+annahoffa@users.noreply.github.com>
2021-11-04 23:00:44 +0100

    Add the note field and fix note fetching logic

--- react-app/src/components/toolbar-manage-note/toolbar-manage-note.component.jsx
+++ react-app/src/components/toolbar-manage-note/toolbar-manage-note.component.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import apiCall from '../../utils/apiCall';
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, makeStyles, TextField, Tooltip, Typography } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import { BiNote } from 'react-icons/bi';
-import { makeStyles, Tooltip } from '@material-ui/core';
 
 
 const useStyles = makeStyles(() => ({
@@ -16,13 +17,79 @@ const useStyles = makeStyles(() => ({
 
 const ToolbarManageNote = () => {
   const { icon } = useStyles();
+  const [noteContents, setNoteContents] = useState('');
+
+  useEffect(() => {
+    apiCall('/api/user/getNote', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(data => setNoteContents(data.text))
+    .catch(error => console.log(error));
+  }, []);
+
+  const editNote = () => {
+    apiCall(`/api/user/editNote?note=${noteContents}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch(error => console.log(error));
+    closeNoteDialog();
+  };
+
+  const handleChange = (event) => {
+    setNoteContents(event.target.value);
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const openNoteDialog = () => {
+    setOpen(true);
+  };
+
+  const closeNoteDialog = () => {
+    setOpen(false);
+  };
 
   return (
-    <Tooltip title='Edytuj notatkę' className={icon}>
-      <IconButton>
-        <BiNote />
-      </IconButton>
-    </Tooltip>
+    <>
+      <Tooltip title='Edytuj notatkę' className={icon}>
+        <IconButton onClick={openNoteDialog}>
+          <BiNote />
+        </IconButton>
+      </Tooltip>
+
+      {/*hidden modification dialog*/}
+      <Dialog open={open} onClose={closeNoteDialog}>
+        <DialogContent>
+          <DialogContentText>
+            Edytuj notatkę
+          </DialogContentText>
+          <Container>
+            <Typography variant='body2' align='center'>Tu możesz zapisać swoje własne uwagi na dany dzień.</Typography>
+            <br />
+            <TextField
+              multiline
+              rows={5}
+              label='Twoja notatka'
+              value={noteContents}
+              onChange={handleChange}
+              placeholder='Tu zacznij pisać...'
+              variant='outlined'
+              fullWidth
+            />
+          </Container>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeNoteDialog}>Anuluj</Button>
+          <Button onClick={editNote}>Zapisz</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
