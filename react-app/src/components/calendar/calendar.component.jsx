0af7e0f9d399f9b1653eb62f5b54e8fab02650bc
annahoffa <60316298+annahoffa@users.noreply.github.com>
2021-11-10 00:34:52 +0100

    Fix exercise fetches on calendar date change

--- react-app/src/components/calendar/calendar.component.jsx
+++ react-app/src/components/calendar/calendar.component.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { format, compareAsc } from 'date-fns';
+import React, { useContext } from 'react';
+import { format } from 'date-fns';
+import apiCall from '../../utils/apiCall';
+import { AuthContext } from '../../appContext/providers';
 
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
@@ -7,17 +9,17 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import { makeStyles } from '@material-ui/core';
-import apiCall from '../../utils/apiCall';
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   calendar: {
     padding: '2em 0',
   },
 }));
 
-export default function Calendar({ setUserProducts }) {
+export default function Calendar({ setUserProducts, setUserExercises }) {
   const { calendar } = useStyles();
+  const authContext = useContext(AuthContext);
   const [date, setDate] = React.useState(format(new Date(), 'yyyy-MM-dd'));
 
   const handleChange = (newDate) => {
@@ -25,14 +27,25 @@ export default function Calendar({ setUserProducts }) {
   };
 
   React.useEffect(() => {
-    apiCall(`/api/user/getUserProducts?date=${date}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(data => setUserProducts(data))
-    .catch(error => console.log(error));
+    if(authContext.isLoggedIn) {
+      apiCall(`/api/user/getUserProducts?date=${date}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(data => setUserProducts(data))
+      .catch(error => console.log(error));
+
+      apiCall(`/api/user/getUserExercises?date=${date}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(data => setUserExercises(data))
+      .catch(error => console.log(error));
+    }
   }, [date]);
 
   return (
