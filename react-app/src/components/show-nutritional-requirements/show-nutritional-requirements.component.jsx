de9224430949f7b454aa86915795a4ca62fb11cd
annahoffa <60316298+annahoffa@users.noreply.github.com>
2021-11-10 05:11:48 +0100

    Add percentage bars for nutritional requirements in dashboard

--- react-app/src/components/show-nutritional-requirements/show-nutritional-requirements.component.jsx
+++ react-app/src/components/show-nutritional-requirements/show-nutritional-requirements.component.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
+import PercentageBar from '../percentage-bar/percentage-bar';
 
-import './show-nutritional-requirements.styles.scss'
+import './show-nutritional-requirements.styles.scss';
+
 
 const ShowNutritionalRequirements = ({ maxValues, currentValues }) => {
   return (
     <div className='daily-requirements-container'>
       <div className='item'>
         <span><b>Kcal:</b></span>
-        <span>{currentValues?.kcal || '\u2014'} / {parseFloat(maxValues?.kcal).toFixed(2)}</span>
+        <div className='percentage-bar'>
+          <span className='numbers'>{currentValues?.kcal || '\u2014'} / {parseFloat(maxValues?.kcal).toFixed(2)}</span>
+          <PercentageBar value={currentValues?.kcal} maxValue={maxValues?.kcal} />
+        </div>
       </div>
       <div className='item'>
         <span><b>Białka:</b></span>
-        <span>{currentValues?.proteins || '\u2014'} / {parseFloat(maxValues?.proteins).toFixed(2)}</span>
+        <div className='percentage-bar'>
+          <span className='numbers'>{currentValues?.proteins || '\u2014'} / {parseFloat(maxValues?.proteins).toFixed(2)}</span>
+          <PercentageBar value={currentValues?.proteins} maxValue={maxValues?.proteins} />
+        </div>
       </div>
       <div className='item'>
         <span><b>Węglowodany:</b></span>
-        <span>{currentValues?.carbohydrates || '\u2014'} / {parseFloat(maxValues?.carbohydrates).toFixed(2)}</span>
+        <div className='percentage-bar'>
+          <span className='numbers'>{currentValues?.carbohydrates || '\u2014'} / {parseFloat(maxValues?.carbohydrates).toFixed(2)}</span>
+          <PercentageBar value={currentValues?.carbohydrates} maxValue={maxValues?.carbohydrates} />
+        </div>
       </div>
       <div className='item'>
         <span><b>Tłuszcze:</b></span>
-        <span>{currentValues?.fat || '\u2014'} / {parseFloat(maxValues?.fat).toFixed(2)}</span>
+        <div className='percentage-bar'>
+          <span className='numbers'>{currentValues?.fat || '\u2014'} / {parseFloat(maxValues?.fat).toFixed(2)}</span>
+          <PercentageBar value={currentValues?.fat} maxValue={maxValues?.fat} />
+        </div>
       </div>
       <div className='item'>
         <span><b>Błonnik:</b></span>
-        <span>{currentValues?.fiber || '\u2014'} / {parseFloat(maxValues?.fiber).toFixed(2)}</span>
+        <div className='percentage-bar'>
+          <span className='numbers'>{currentValues?.fiber || '\u2014'} / {parseFloat(maxValues?.fiber).toFixed(2)}</span>
+          <PercentageBar value={currentValues?.fiber} maxValue={maxValues?.fiber} />
+        </div>
       </div>
     </div>
   );
